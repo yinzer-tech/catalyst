789a384100356676e5bd48060f4eecd9193ee84a
Shawn <sjonnet@gmail.com>
2025-03-17 12:35:24 -0400

    Adjustments for client link

--- link.jsx
+++ link.jsx
@@ -1,11 +1,11 @@
 import * as Headless from '@headlessui/react'
-import NextLink from 'next/link'
 import { forwardRef } from 'react'
+import { Link as RouterLink } from "react-router";
 
 export const Link = forwardRef(function Link(props, ref) {
   return (
     <Headless.DataInteractive>
-      <NextLink {...props} ref={ref} />
+      {props.href.includes('http') ? <a {...props} ref={ref} /> : <RouterLink {...props} to={props.href} ref={ref} />}
     </Headless.DataInteractive>
   )
 })
